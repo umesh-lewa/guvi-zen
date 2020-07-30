aa1ddba4bfa9c90a06d88b77523a2debf7f6a6ea
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-07-30 22:08:07 +0530

    add restCountries filter tasks

--- Week_2/Day_9/RestCountriesWithFilterReduceForEach.js
+++ Week_2/Day_9/RestCountriesWithFilterReduceForEach.js
@@ -10,11 +10,54 @@ myrequest.onload = function() {
 //  console.log(this.response);
 var data = JSON.parse(this.response);
 
-var asianCountries = data.filter(function(data){
-	return data.region == "asia"; 
+var asianCountries = data.filter(function(el) {
+      return el.region == "Asia"
+  });
+console.log(asianCountries);
+
+
+var popultionLessThan2L = data.filter(function(country){
+	return country.population <= 200000; 
 });
+console.log(popultionLessThan2L);
 
-//console.log(asianCountries);
 
+var totalPopulation = data.reduce(function (accumulator, country) {
+  return accumulator + country.population;
+}, 0);
+console.log(totalPopulation);
+
+
+var add1LToPopulation = data.map(function (officer) {
+  return officer.population = officer.population + 10000;
+});
+console.log(add1LToPopulation);
 console.log(data);
+
+
+var upperCaseCapitals = data.map(function (country) {
+  return country.capital = country.capital.toUpperCase();
+});
+console.log(upperCaseCapitals);
+console.log(data)
+
+
+data.forEach((element) => {
+	console.log(element["name"]);
+	console.log(element["capital"]);
+	console.log(element["flag"]);
+})
+
+
+var allAsianCountries = data.filter(function(el) {
+      return el.region == "Asia"
+  });
+
+var totalPopulationInAsia = allAsianCountries.reduce(function (accumulator, country) {
+  return accumulator + country.population;
+}, 0);
+
+console.log(totalPopulationInAsia);
+
+//console.log(data);
 }
