565da7c426b60b828e68a667727de6cd2878c005
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-08-04 08:25:46 +0530

    small changes

---
+++ Week_2/Day_10/SpreadOperatorTasks.js
@@ -0,0 +1,45 @@
+const person = {
+    firstName: "Suresh",
+    lastName: "Raina",
+    age: 27,
+    gender: "M"
+  }
+  
+let first = person.firstName;
+let age = person.age;
+let gender = person.gender;
+
+console.log(first); 
+console.log(age); 
+console.log(gender); 
+
+const person = {
+    firstName: "Suresh",
+    lastName: "Raina",
+    age: 27,
+    gender: "M"
+  }
+  
+const { firstName, lastName, age, gender } = person;
+  
+console.log(firstName);
+console.log(age);
+console.log(gender);
+  
+var person1 = {
+    firstName:"Sachin",
+    lastName:"Tendulkar",
+    age: 35,
+    gender:"M"
+}
+
+function joinFullName({firstName,lastName}) {
+    return firstName + '->' + lastName;
+}
+  
+console.log(joinFullName(person1));
+
+var list = [ 1, 2, 3 ];
+var [ a, b ] = list;
+[ b, a ] = [ a, b ]
+console.log(a, b)
