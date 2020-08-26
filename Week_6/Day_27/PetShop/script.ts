c713ffd233a6c52f956812500067000013f23def
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-08-26 22:11:03 +0530

    add incoming request

--- Week_6/Day_27/PetShop/script.ts
+++ Week_6/Day_27/PetShop/script.ts
@@ -1,10 +1,12 @@
 class PetAvailability {
-	availablePets: Pet [] = [];
+	public availablePets: Pet [] = [];
 	enquiries: PetRequest [] = [];
 
 	dogCount = 0;
 	catCount = 0;
 	parrotCount = 0;
+
+	availSwitch = true;
 
 	constructor(){
 		
@@ -18,11 +20,14 @@ class PetAvailability {
 
 	insertPets(){
 		for (let index = 0; index < 5; index++) {
-			this.availablePets.push(new Pet(`Dog ${index + 1}`,'Dog',this.getRandomInt(10000,19999)));
-
-			this.availablePets.push(new Pet(`Cat ${index + 1}`,'Cat',this.getRandomInt(20000,29999)));
-
-			this.availablePets.push(new Pet(`Parrot ${index + 1}`,'Parrot',this.getRandomInt(30000,39999)));
+			if(index%2 == 0){
+				this.availSwitch = true;
+			}else{
+				this.availSwitch = false;
+			}
+			this.availablePets.push(new Pet(`Dog ${index + 1}`,'Dog',this.getRandomInt(10000,19999),this.availSwitch));
+			this.availablePets.push(new Pet(`Cat ${index + 1}`,'Cat',this.getRandomInt(20000,29999),this.availSwitch));
+			this.availablePets.push(new Pet(`Parrot ${index + 1}`,'Parrot',this.getRandomInt(30000,39999),this.availSwitch));
 		  }
 	}
 
@@ -30,7 +35,8 @@ class PetAvailability {
 		this.dogCount = 0;
 		this.catCount = 0;
 		this.parrotCount = 0;
-
+		console.log("available pets length : "+this.availablePets.length);
+		
 		this.availablePets.forEach((element) => {
 			if(element.type == "Dog"){
 				this.dogCount++;
@@ -45,24 +51,80 @@ class PetAvailability {
 		console.log("catCount : "+this.catCount);
 		console.log("parrotCount : "+this.parrotCount);
 	}
+
+	public getAvailablePets(){
+		return this.availablePets;
+	}
 }
 
-class PetRequest{
+class PetRequest extends PetAvailability{
 	type:string;
-	
-	constructor(){
+	totalRequestedPets: Pet [] = [];
+	//totalRequestedPets: any[];
+	tempPet;
+	element;
+	availPets = this.availablePets;
+	totalMatchedPets:number;
+	totalMatchedPetsAvailable:number;
 
+	constructor(){
+		super();
 	}
 
-	insertIncomingEnquiries(){
-
+	insertIncomingEnquiries(enquiry:Pet [] = []){
+		this.totalRequestedPets.push(...enquiry);
 	}
 
 	getStatus(){
-		for(let i = 0  ; i  < 5 ; i ++){
+		console.log("inside get status method");
+		//this.availPets = this.availablePets;
+		//console.log(this.availablePets.length);
+		//console.log(this.availablePets);
+		console.log(this.availPets.length);
+		console.log(this.availPets);
+		for(let i = 0  ; i  < 5 ; i ++){				
+				this.element = this.totalRequestedPets[i];
+				//this.availPets = this.getAvailablePets;
+				
+				console.log("iterating through requsted pets");			
+				//console.log(this.availPets.length);
 
-			console.log();
+				for(let j = 0 ; j < this.availablePets.length ; j++) {
+					console.log("iterating through each evailable pet");
+					if(this.availablePets[j].name == this.element.name){
+						this.tempPet = this.element;
+						console.log("This pet is there in shop");
+						if(this.tempPet.currentStatus){
+							console.log("This pet is available right now");
+						}else{
+							console.log("This pet is not available right now");
+						}
+						console.log("current status : "+this.element.currentStatus);
+					}else{
+						console.log("This pet is not there in shop");
+					}
+				}		
 		}
+	}
+
+	mapRequestData(){
+		this.totalRequestedPets.forEach(element => {
+				for(let j = 0 ; j < this.availablePets.length ; j++) {
+					if(this.availablePets[j].name == this.element.name){
+						this.tempPet = this.element;
+						this.totalMatchedPets++;
+						if(this.tempPet.currentStatus){
+							this.totalMatchedPetsAvailable++;
+						}else{
+							//do nothing
+						}
+					}else{
+						//do nothing
+					}
+				}
+				console.log(this.totalMatchedPets);
+				console.log(this.totalMatchedPetsAvailable);
+		});
 	}
 }
 
@@ -70,11 +132,13 @@ class Pet{
 	name:string;
 	type:string;
 	RFID:number;
+	currentStatus:boolean;
 
-	constructor(petName:string,petType:string,rfid:number){
+	constructor(petName:string,petType:string,rfid:number,status:boolean){
 		this.name = petName;
 		this.type = petType;
 		this.RFID = rfid;
+		this.currentStatus = status;
 	}
 }
 
@@ -82,3 +146,27 @@ let avail = new PetAvailability();
 avail.insertPets();
 
 avail.getCountOfDifferentPets();
+
+let request = new PetRequest();
+
+let insertPetRequests: Pet [] = [];
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let index = 0; index < 5; index++) {
+	let availSwitch = true;
+	if(index%2 == 0){
+		availSwitch = true;
+	}else{
+		availSwitch = false;
+	}
+	insertPetRequests.push(new Pet(`Dog ${index + 1}`,'Dog',getRandomInt(10000,19999),availSwitch));
+}
+
+request.insertIncomingEnquiries(insertPetRequests);
+
+request.getStatus();
