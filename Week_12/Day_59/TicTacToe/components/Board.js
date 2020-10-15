f948b10fb28ccdc974a1b832fc334096afa0da5d
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-10-15 16:28:23 +0530

    css changes

--- Week_12/Day_59/TicTacToe/components/Board.js
+++ Week_12/Day_59/TicTacToe/components/Board.js
@@ -2,10 +2,10 @@ import React from "react";
 import Square from './Square';
 
 const style = {
-    border: '4px solid darkblue',
+    border: '4px solid black',
     borderRadius: '10px',
-    width: '250px',
-    height: '250px',
+    width: '500px',
+    height: '500px',
     margin: '0 auto',
     display: 'grid',
     gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
